f70431d8e105a480226597a737655167b16cf40a
elly1107 <elly1107011@gmail.com>
2024-08-03 04:27:24 +0900

    java-mid1

--- src/enumeration/ex3/EnumMethodMain.java
+++ src/enumeration/ex3/EnumMethodMain.java
@@ -6,12 +6,15 @@ public class EnumMethodMain {
     public static void main(String[] args) {
         //모든 enum 반환
         Grade[] values = Grade.values();
+        System.out.println("values = " + values);
         System.out.println("values = " + Arrays.toString(values));
         for (Grade value : values) {
             System.out.println("name = " + value.name() + ", ordinal = " + value.ordinal());
         }
 
         //String -> ENUM 변환
-
+        String input = "GOLD";
+        Grade gold = Grade.valueOf(input);
+        System.out.println("gold = " + gold);
     }
 }
