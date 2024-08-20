978de3ed2759893ef3ff6e8421b83ed496f07fc6
elly1107 <elly1107011@gmail.com>
2024-08-21 01:43:35 +0900

    java-mid1

--- src/exception/basic/unchecked/UncheckedThrowMain.java
+++ src/exception/basic/unchecked/UncheckedThrowMain.java
@@ -3,7 +3,11 @@ package exception.basic.unchecked;
 public class UncheckedThrowMain {
     public static void main(String[] args) {
         Service service = new Service();
-        service.callThrow();
+        try {
+            service.callThrow();
+        } catch (MyUncheckedException e) {
+            System.out.println("오류메세지: "+e.getMessage());
+        }
         System.out.println("정상 종료");
     }
 }
